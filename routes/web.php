526c2e7c044492f6d339b108cebb82096e6b78be
mohsaleh80 <mohsaleh80@gmail.com>
2021-12-06 17:50:33 +0200

    PagesController

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-*/
 
 Route::get('/', function () {
     return view('index');
 });
+*/
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::get('/about', 'App\Http\Controllers\PagesController@about');
+Route::get('/services', 'App\Http\Controllers\PagesController@services');
